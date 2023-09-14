0ca863039cb82f0540ce1a47b8c225a6249d14dc
yyhuii <daizero@126.com>
2023-09-14 14:42:27 +0800

    Update README.md

--- src/ui/tool_bar.py
+++ src/ui/tool_bar.py
@@ -3,8 +3,9 @@ Author:Qychui
 DATE:2023/9/13 14:26
 File:tool_bar.py
 """
+from PyQt6 import QtCore
 from PyQt6.QtGui import QAction, QIcon
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QFrame, QSplitter
 
 
 class ToolWindows(QMainWindow):
@@ -13,6 +14,8 @@ class ToolWindows(QMainWindow):
         super().__init__()
 
     def initUI(self):
+
+
         # 自定义添加状态栏
         self.save_act = QAction(QIcon('icon/Save.png'), 'Save', self)
         self.save_act.setStatusTip('保存程序')
@@ -32,4 +35,41 @@ class ToolWindows(QMainWindow):
         self.toolbar = self.addToolBar('工具栏')
 
         # 每次写完后需要往toolbar中添加实例
-        self.toolbar.addActions((self.save_act, self.open_act,self.edit_act,self.quit_act))
+        self.toolbar.addActions((self.save_act, self.open_act, self.edit_act, self.quit_act))
+
+        self.topLeft = QFrame(self)
+        self.topLeft.setFrameShape(QFrame.Shape.StyledPanel)
+        self.topLeft.setBaseSize(200, 300)
+        self.topLeft.setMinimumWidth(50)
+
+        self.topMiddle = QFrame(self)
+        self.topMiddle.setFrameShape(QFrame.Shape.StyledPanel)
+        self.topMiddle.setBaseSize(600, 300)
+        self.topMiddle.setMinimumWidth(300)
+
+        self.topRight = QFrame(self)
+        self.topRight.setFrameShape(QFrame.Shape.StyledPanel)
+        self.topRight.setBaseSize(600, 300)
+        self.topRight.setMinimumWidth(300)
+
+        self.botRight = QFrame(self)
+        self.botRight.setFrameShape(QFrame.Shape.StyledPanel)
+        self.botRight.setBaseSize(600, 150)
+        self.botRight.setMinimumHeight(50)
+
+        # 实例化QSplitter并将初始方向设置为水平
+        self.topSplitter = QSplitter(self)
+
+        # 向topSplitter中添加控件并设置大小
+        self.topSplitter.addWidget(self.topLeft)
+        self.topSplitter.addWidget(self.topMiddle)
+        self.topSplitter.addWidget(self.topRight)
+        self.topSplitter.setMinimumHeight(400)
+        self.topSplitter.setFixedHeight(600)
+
+        self.botSplitter = QSplitter(self)
+        self.botSplitter.setOrientation(QtCore.Qt.Orientation.Vertical)
+        self.botSplitter.addWidget(self.topSplitter)
+        self.botSplitter.addWidget(self.botRight)
+
+        self.setCentralWidget(self.botSplitter)
--- tests/qsplitter.py
+++ tests/qsplitter.py
@@ -1,5 +1,6 @@
 import sys
 
+from PyQt6 import QtCore
 from PyQt6.QtGui import QIcon, QAction
 from PyQt6.QtWidgets import QApplication, QWidget, QSplitter, QTextEdit, QVBoxLayout, QFrame, QMainWindow
 
@@ -13,30 +14,31 @@ class SplitterExample(QMainWindow, QWidget):
         # 实例化两个QFrame控件
         topLeft = QFrame(self)
         topLeft.setFrameShape(QFrame.Shape.StyledPanel)
-        topLeft.setBaseSize(400, 400)
-        topLeft2 = QFrame(self)
-        topLeft2.setFrameShape(QFrame.Shape.StyledPanel)
-        topLeft2.setBaseSize(400, 400)
-        bottom = QFrame(self)
-        bottom.setFrameShape(QFrame.Shape.StyledPanel)
-        bottom.setMinimumSize(300, 200)
-        bottom.setBaseSize(400, 400)
+        topMiddle = QFrame(self)
+        topMiddle.setFrameShape(QFrame.Shape.StyledPanel)
+        topRight = QFrame(self)
+        topRight.setFrameShape(QFrame.Shape.StyledPanel)
+        topRight.setMinimumSize(300, 200)
+
 
         # 实例化QSplitter并将初始方向设置为水平
-        splitter1 = QSplitter(self)
+        topSplitter = QSplitter(self)
+        topSplitter.setOrientation(QtCore.Qt.Orientation.Vertical)
 
         # 向splitter1中添加控件并设置它们的初始大小
-        splitter1.addWidget(topLeft)
-        splitter1.addWidget(topLeft2)
-        splitter1.addWidget(bottom)
-        splitter1.setSizes([50, 100])
+        topSplitter.addWidget(topLeft)
+        topSplitter.addWidget(topMiddle)
+        topSplitter.addWidget(topRight)
+        topSplitter.setStretchFactor(0,1)
+        topSplitter.setStretchFactor(1,1)
+        topSplitter.setStretchFactor(2,1)
 
         # 实例化另一个QSplitter并设置其方向为垂直
-        splitter2 = QSplitter(self)
-        splitter2.addWidget(splitter1)
-        # splitter2.addWidget(bottom)
+        botSplitter = QSplitter(self)
+        botSplitter.addWidget(topSplitter)
 
-        self.setCentralWidget(splitter2)
+
+        self.setCentralWidget(botSplitter)
 
         exitAct = QAction(QIcon('../Icon/RandomUser.ico'), 'Exit', self)
         exitAct.setShortcut('Ctrl+Q')
